b33e7116756dc5af98f952a40902c040aa61273c
ACherniske <apcherniske@gmail.com>
2026-04-21 16:53:42 -0400

    feat: Add testbench template

---
+++ tb/Module_tb.v
@@ -0,0 +1,14 @@
+// Boilerplate template for /tb/Module_tb.v
+`timescale 1ns / 1ps
+
+module Module_tb;
+    // 1. Define signals (reg for inputs, wire for outputs)
+    // 2. Instantiate Unit Under Test (UUT)
+    // 3. Logic to drive inputs
+    
+    initial begin
+        // ALWAYS use the filename as the VCD name
+        $dumpfile("Module.vcd"); 
+        $dumpvars(0, Module_tb);
+    end
+endmodule
